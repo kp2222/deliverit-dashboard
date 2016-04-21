2353d0004773db789ea3736282c9b2d6a4ee309c
krishnaprasad <kp2222@gmail.com>
2016-04-21 23:44:26 +0530

    new format for dashboard config

--- src/cljs/deliverit_dashboard/core.cljs
+++ src/cljs/deliverit_dashboard/core.cljs
@@ -5,10 +5,26 @@
 
 
 
-(def layout (grid/grid-layout [[s/simple-text-widget {:name :widget-one :text "Hello" :pos {:x 0 :y 0 :h 1 :w 2 }}]
-                               [s/simple-text-widget {:name :widget-two :text "world!!" :pos {:x 0 :y 1 :h 1 :w 2}}]]))
+(def dashboard {:layout :grid-layout
+                :widgets [
+                          {
+                           :type :simple-text
+                           :name :first-widget
+                           :data-source :bangalore-weather
+                           :title "the second one"
+                           :text "Hello World!!"
+                           :layout-opts {:position {:x 0 :y 1 :h 1 :w 2 }}}
+
+                          {
+                           :type :simple-text
+                           :name :second-widget
+                           :data-source :kochi-weather
+                           :title "the second one"
+                           :text "Hello World!!"
+                           :layout-opts {:position {:x 0 :y 1 :h 1 :w 2 }}}]})
 
 
-(d/render-dashboard layout "app")
+
+(d/start-dashboard dashboard "app")
 
 
