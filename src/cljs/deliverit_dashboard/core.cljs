9628c43f8cefedd20b0f043953bf874abcaee0c2
krishnaprasad <kp2222@gmail.com>
2016-05-16 18:01:26 +0530

    use mobile resposive layout!

--- src/cljs/deliverit_dashboard/core.cljs
+++ src/cljs/deliverit_dashboard/core.cljs
@@ -1,12 +1,12 @@
 (ns deliverit-dashboard.core
   (:require [dashboard-clj.core :as d]
-            [dashboard-clj.layouts.grid-layout :as grid]
+            [dashboard-clj.layouts.grid-layout-responsive :as grid]
             [dashboard-clj.widgets.simple-text :as s]
             [dashboard-clj.widgets.github-repo-stats]
             [re-frame.core :as rf]))
 
-(def dashboard {:layout :grid-layout
-                :layout-opts { :rowHeight 500 :cols 6}
+(def dashboard {:layout :responsive-grid-layout
+                :layout-opts {:cols {:lg 6 :md 4 :sm 2 :xs 1 :xxs 1}}
                 :widgets [
                           {
                            :type :github-repo-stats
@@ -15,7 +15,9 @@
                            :repo-name "Dashboard (Clojure)"
                            :title "commits"
                            :text "commit history"
-                           :layout-opts {:position {:x 0 :y 0  :h 1 :w 2 }}
+                           :layout-opts {:position {:lg {:x 0 :y 0  :h 3 :w 2 :static true }
+                                                    :md {:x 0 :y 0 :h 3 :w 2}
+                                                    :sm {:x 0 :y 0 :h 3 :w 2 :static true}}}
                            :style {:background-image "linear-gradient(120deg, #b8ce9a, #08701a)"
                                    :background-color "#b8ce9a"
                                    :font-family "Times New Roman, Times, serif"
@@ -26,7 +28,9 @@
                            :data-source :onemdm-library
                            :repo-name  "One MDM"
                            :text "commit history"
-                           :layout-opts {:position {:x 2 :y 0  :h 1 :w 2 }}
+                           :layout-opts {:position {:lg {:x 2 :y 0  :h 3 :w 2 }
+                                                    :md {:x 2 :y 0 :h 3 :w 2}
+                                                    :sm {:x 0 :y 0 :h 3 :w 2 :static true}}}
                            :style {:background-image "linear-gradient(120deg, #155799, #159957)"
                                    :background-color "#ffffff"
                                    :font-family "Times New Roman, Times, serif"
@@ -37,7 +41,9 @@
                            :data-source :multunus-website
                            :repo-name  "Multunus Website"
                            :text "history"
-                           :layout-opts {:position {:x 4 :y 0  :h 1 :w 2 }}
+                           :layout-opts {:position {:lg {:x 4 :y 0  :h 3 :w 2 }
+                                                    :md {:x 0 :y 3 :h 3 :w 2}
+                                                    :sm {:x 0 :y 0 :h 3 :w 2 :static true}}}
                            :style {:background-image "linear-gradient(120deg, #f4164d, #7f1010)"
                                    :font-family "Times New Roman, Times, serif"
                                    :font-size "20"}}]})
